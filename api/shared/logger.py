57af2076da073196608c0c708911e30ad11425ad
volodymyrb <volodymyrb@fornova.net>
2021-02-07 14:34:13 +0200

    logging

---
+++ api/shared/logger.py
@@ -0,0 +1,25 @@
+import sys
+import logging
+
+LOG_BASE_NAME = 'movie'
+LOG_FORMAT = '%(asctime)s %(levelname)-7s %(name)-15s: %(message)s'
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+def logger_configure(level: str = 'DEBUG', root_level: str = 'WARNING') -> None:
+    """Configure console logger."""
+    log_handler = logging.StreamHandler(stream=sys.stdout)
+
+    log_formatter = logging.Formatter(fmt=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
+    log_handler.setFormatter(log_formatter)
+
+    # root logger
+    logging.getLogger('').addHandler(log_handler)
+    logging.getLogger('').setLevel(root_level)
+
+    # local logger
+    logging.getLogger(LOG_BASE_NAME).setLevel(level)
+
+
+def get_logger(name: str) -> logging.Logger:
+    return logging.getLogger(f'{LOG_BASE_NAME}.{name}')
--- movieapi.py
+++ movieapi.py
@@ -1,19 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
 
+from api.shared import logger
 from database import database, configs
 
+logger.logger_configure()
+log = logger.get_logger('Movie')
 
 app = FastAPI()
 
-
 @app.on_event('startup')
 async def startup():
+    log.info('Setting up database...')
     await database.setup(config=configs.MYSQL_CONFIG)
 
 
 @app.on_event('shutdown')
 async def startup():
+    log.info('Shutting down database...')
     await database.shutdown()
 
 
